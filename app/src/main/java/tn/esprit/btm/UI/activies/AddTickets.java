ce3320959fed9d54885df03519ffa3c6cef58951
rania05 <rania.arfaoui.1@esprit.tn>
2020-12-12 17:08:19 +0100

    added map , problem in acheter ticket

---
+++ app/src/main/java/tn/esprit/btm/UI/activies/AddTickets.java
@@ -0,0 +1,14 @@
+package tn.esprit.btm.UI.activies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTickets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tickets);
+    }
+}
